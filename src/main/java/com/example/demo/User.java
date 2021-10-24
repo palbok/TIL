2160302fc74d899b1eb2354def2f170773e69deb
palbok <palbok92@gmail.com>
2021-10-24 16:13:18 +0900

    add jpa, h2db

---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class User {
+
+    @Id
+    private Long id;
+
+    private String name;
+}
