722a485b162c8c8eca51fa32f57583407df61ef8
palbok <palbok92@gmail.com>
2021-10-25 23:58:37 +0900

    lombok constructor injection

---
+++ src/main/java/com/example/demo/UserController.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+}
