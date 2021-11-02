daec185b4bea238596a549b232cecdf544345382
palbok <palbok92@gmail.com>
2021-11-02 23:45:17 +0900

    add user api

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,9 +1,30 @@
 package com.example.demo.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    @NonNull
+    private final UserService userService;
+
+    @RequestMapping(value = "",method = RequestMethod.POST)
+    public void add(@RequestBody User user){
+        userService.save(user);
+    }
+
+    @RequestMapping(value = "",method = RequestMethod.GET)
+    public List<User> list(){
+        return userService.findAll();
+    }
+
+
 }
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,8 +1,11 @@
 package com.example.demo.service;
 
+import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -10,5 +13,11 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    public User save(User user){
+        return userRepository.save(user);
+    }
 
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
 }
